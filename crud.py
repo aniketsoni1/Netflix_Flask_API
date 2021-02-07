00266962485fbabcd57be72f5281e986dfe433a5
Aniket Soni <47126963+aniketsoni1@users.noreply.github.com>
2021-02-07 12:33:00 -0500

    updated GET, POST, PUT, DELETE request
    updated GET, POST, PUT, DELETE request

--- crud.py
+++ crud.py
@@ -30,3 +30,93 @@ class ShowsDBCRUD:
             """)
         rows = cur.fetchall()
         return rows
+
+    def search_shows_by_title(self, search_phrase):
+        print('search_shows_by_title crud.py')
+        cur = self.con.cursor()
+        cur.execute(f"""
+            SELECT
+                *
+            FROM shows s
+            WHERE s.title LIKE '%{search_phrase}%';
+            """)
+        rows = cur.fetchall()
+        print(rows)
+        return rows
+
+    def filter_shows_by_country(self, country, limit, offset):
+        print('filter by country crud.py')
+        cur = self.con.cursor()
+        cur.execute(f"""
+            SELECT
+                *
+            FROM shows s
+            WHERE s.country LIKE '%{country}%'
+            LIMIT {limit}
+            OFFSET {offset};
+            """)
+        rows = cur.fetchall()
+        print(rows)
+        return rows
+
+    def filter_shows_by_rating(self, rating, limit, offset):
+        print('filter by rating crud.py')
+        cur = self.con.cursor()
+        cur.execute(f"""
+            SELECT
+                *
+            FROM shows s
+            WHERE s.rating = '{rating}'
+            LIMIT {limit}
+            OFFSET {offset};
+            """)
+        rows = cur.fetchall()
+        print(rows)
+        return rows
+
+    def filter_shows_by_release_year(self, release_year, limit, offset):
+        print('filter by country crud.py')
+        cur = self.con.cursor()
+        cur.execute(f"""
+            SELECT
+                *
+            FROM shows s
+            WHERE s.release_year = {release_year}
+            LIMIT {limit}
+            OFFSET {offset};
+            """)
+        rows = cur.fetchall()
+        print(rows)
+        return rows
+
+    def update_show_type(self, show_type, show_id):
+        print('update_show_type crud.py')
+        cur = self.con.cursor()
+        cur.execute(f"""
+            UPDATE shows
+            SET type = '{show_type}'
+            WHERE show_id = {show_id};
+            """)
+        self.con.commit()
+
+    def delete_show_by_id(self, show_id):
+        print('delete_show_by_id crud.py')
+        cur = self.con.cursor()
+        cur.execute(f"""
+            DELETE FROM shows
+            WHERE show_id = {show_id};
+            """)
+        self.con.commit()
+
+
+    def insert_show(self, show_id, type, title, director, cast, country,
+                    date_added, release_year, rating, duration, listed_in, description):
+        print('insert_show crud.py')
+        cur = self.con.cursor()
+        cur.execute(f"""
+            INSERT INTO shows (show_id, type, title, director, "cast", country,
+             date_added, release_year, rating, duration, listed_in, description)
+            VALUES ('{show_id}', '{type}', '{title}', '{director}', '{cast}', '{country}',
+             '{date_added}', {release_year}, '{rating}', '{duration}', '{listed_in}', '{description}');
+            """)
+        self.con.commit()
